fedf723526886da79f3bb1f1c1fb83ea54c700cc
Tharindu Jananjaya Bandara <tjbro2003@gmail.com>
2026-07-13 03:42:10 +0530

    Delete app directory

--- app/not-found.tsx
+++
@@ -1,2 +0,0 @@
-import Link from "next/link";
-export default function NotFound(){return <main className="not-found"><span>404</span><h1>This page drifted out of orbit.</h1><p>The route you requested doesn&apos;t exist.</p><Link href="/">Return home →</Link></main>}
